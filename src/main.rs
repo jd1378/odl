ef95db41e8b396222f8f6868c026653ef571b755
Javad Mnjd <javad.mnjd@hotmail.com>
2025-07-14 13:21:45 +0330

    fix: implement simple progress bar

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::{path::PathBuf, time::Duration};
 
 use async_trait::async_trait;
 use clap::Parser;
+use indicatif::{ProgressState, ProgressStyle};
 use odl::{
     Download,
     conflict::{
@@ -16,6 +17,9 @@ use tokio::{self, io::AsyncBufReadExt};
 mod args;
 use args::Args;
 use futures::future::join_all;
+use tracing::instrument;
+use tracing_indicatif::IndicatifLayer;
+use tracing_subscriber::{Layer, layer::SubscriberExt, util::SubscriberInitExt};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum DownloadType {
@@ -60,10 +64,110 @@ impl SaveConflictResolver for CliResolver {
     }
 }
 
+pub const CHARS_FADE_IN: &'static str = "█▓▒░  ";
+pub const TEMPLATE_BAR_WITH_POSITION: &'static str =
+    "{bar:40.blue} {pos:>}/{len} ({percent}%) eta {eta_precise:.blue}";
+
+fn elapsed_subsec(state: &ProgressState, writer: &mut dyn std::fmt::Write) {
+    let seconds = state.elapsed().as_secs();
+    let sub_seconds = (state.elapsed().as_millis() % 1000) / 100;
+    let _ = writer.write_str(&format!("{}.{}s", seconds, sub_seconds));
+}
+
 #[tokio::main]
 async fn main() -> Result<(), OdlError> {
     let args = Args::parse();
+    let indicatif_layer = IndicatifLayer::new().with_progress_style(
+        ProgressStyle::with_template(
+            "{color_start}{span_child_prefix} {span_name} -- {bar:40.blue} {pos:>}/{len} ({percent}%) eta {eta_precise:.blue}{color_end}",
+        )
+        .unwrap().with_key(
+            "elapsed_subsec",
+            elapsed_subsec,
+        ).with_key(
+            "color_start",
+            |state: &ProgressState, writer: &mut dyn std::fmt::Write| {
+                if state.elapsed() > Duration::from_secs(6) && state.len().is_some_and(|l| l > 0) && state.pos() == 0 {
+                    let _ = write!(writer, "\x1b[{}m", 3 + 30); 
+                }  
+            },
+        ).with_key(
+            "color_end",
+            |_: &ProgressState, writer: &mut dyn std::fmt::Write| {
+                let _ = write!(writer, "\x1b[0m");
+            },
+        )
+    ).with_span_child_prefix_symbol("↳ ").with_span_child_prefix_indent(" ");
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::fmt::layer()
+                .with_writer(indicatif_layer.get_stderr_writer())
+                .with_filter(tracing_subscriber::filter::LevelFilter::INFO),
+        )
+        .with(indicatif_layer)
+        .init();
 
+    let dlm = build_download_manager(&args)?;
+
+    let mut download_type = determine_download_type(&args)?;
+
+    if let DownloadType::FileAtUrl(url) = &download_type {
+        let path = download_remote_file(&dlm, url.clone()).await?;
+        download_type = DownloadType::File(Box::new(path));
+    }
+
+    let resolver = CliResolver { force: args.force };
+
+    // todo: stream file, as processing a large file in advance is not a good idea
+    let mut urls = Vec::new();
+    match &download_type {
+        DownloadType::Url(url) => {
+            urls.push(url.clone());
+        }
+        DownloadType::File(path) => {
+            let file = tokio::fs::File::open(&**path).await?;
+            let reader = tokio::io::BufReader::new(file);
+            let mut lines = tokio::io::BufReader::new(reader).lines();
+            while let Some(line) = lines.next_line().await? {
+                let trimmed = line.trim();
+                if trimmed.is_empty() || trimmed.starts_with('#') || trimmed.starts_with("//") {
+                    continue;
+                }
+                match Url::parse(trimmed) {
+                    Ok(url) => urls.push(url),
+                    Err(e) => {
+                        println!("Skipping invalid URL '{}': {}", trimmed, e);
+                    }
+                }
+            }
+        }
+        DownloadType::FileAtUrl(_) => {
+            panic!("FileAtUrl should have been handled already");
+        }
+    }
+
+    let mut futures = Vec::new();
+    for url in urls.into_iter() { 
+        let fut = dlm.evaluate_and_download_queued(url, None, &resolver, &resolver);
+        futures.push(fut);
+    }
+
+    let results: Vec<Result<_, OdlError>> = join_all(futures).await;
+    for res in results {
+        if let Err(e) = res {
+            eprintln!("Error: {}", e);
+            #[cfg(debug_assertions)]
+            {
+                eprintln!("{e:?}");
+            }
+        }
+    }
+
+    Ok(())
+}
+
+#[instrument(skip(args), name = "Warming up odl...")]
+fn build_download_manager(args: &Args) -> Result<DownloadManager, OdlError> {
     let proxy = if let Some(proxy_str) = &args.proxy {
         match Proxy::all(proxy_str) {
             Ok(p) => Some(p),
@@ -83,13 +187,13 @@ async fn main() -> Result<(), OdlError> {
         .max_concurrent_downloads(args.max_concurrent_downloads)
         .max_retries(args.retry)
         .wait_between_retries(Duration::from_secs_f32(args.waitretry))
-        .user_agent(args.user_agent)
+        .user_agent(args.user_agent.clone())
         .randomize_user_agent(args.randomize_user_agent)
         .proxy(proxy)
         .use_server_time(args.use_server_time)
         .accept_invalid_certs(args.accept_invalid_certs);
 
-    if let Some(download_dir) = args.temp_download_dir {
+    if let Some(download_dir) = args.temp_download_dir.clone() {
         builder.download_dir(download_dir);
     }
 
@@ -121,7 +225,12 @@ async fn main() -> Result<(), OdlError> {
         builder.headers(Some(headers_map));
     }
 
-    let mut download_type = match Url::parse(&args.input) {
+    Ok(builder.build()?)
+}
+
+#[instrument(skip(args), name = "Determining download type")]
+fn determine_download_type(args: &Args) -> Result<DownloadType, OdlError> {
+    Ok(match Url::parse(&args.input) {
         Ok(url) => {
             if args.remote_list {
                 DownloadType::FileAtUrl(url)
@@ -144,60 +253,7 @@ async fn main() -> Result<(), OdlError> {
                 });
             }
         }
-    };
-
-    let dlm: DownloadManager = builder.build()?;
-
-    if let DownloadType::FileAtUrl(url) = &download_type {
-        let path = download_file_anyway(&dlm, url.clone()).await?;
-        download_type = DownloadType::File(Box::new(path));
-    }
-
-    let resolver = CliResolver { force: args.force };
-
-    let mut urls = Vec::new();
-    match &download_type {
-        DownloadType::Url(url) => {
-            urls.push(url.clone());
-        }
-        DownloadType::File(path) => {
-            let file = tokio::fs::File::open(&**path).await?;
-            let reader = tokio::io::BufReader::new(file);
-            let mut lines = tokio::io::BufReader::new(reader).lines();
-            while let Some(line) = lines.next_line().await? {
-                let trimmed = line.trim();
-                if trimmed.is_empty() || trimmed.starts_with('#') || trimmed.starts_with("//") {
-                    continue;
-                }
-                match Url::parse(trimmed) {
-                    Ok(url) => urls.push(url),
-                    Err(e) => {
-                        println!("Skipping invalid URL '{}': {}", trimmed, e);
-                    }
-                }
-            }
-        }
-        DownloadType::FileAtUrl(_) => {
-            panic!("FileAtUrl should have been handled already");
-        }
-    }
-
-    let mut futures = Vec::new();
-    for url in urls.into_iter() {
-        let dlm = &dlm;
-        let resolver = &resolver;
-        let fut = dlm.evaluate_and_download_queued(url, None, resolver, resolver);
-        futures.push(fut);
-    }
-
-    let results: Vec<Result<_, OdlError>> = join_all(futures).await;
-    for res in results {
-        if let Err(e) = res {
-            eprintln!("Download failed: {}", e);
-        }
-    }
-
-    Ok(())
+    })
 }
 
 struct ForcedResolver;
@@ -221,7 +277,8 @@ impl SaveConflictResolver for ForcedResolver {
     }
 }
 
-async fn download_file_anyway(dlm: &DownloadManager, url: Url) -> Result<PathBuf, OdlError> {
+#[instrument(skip(dlm), name = "Downloading remote file containing links")]
+async fn download_remote_file(dlm: &DownloadManager, url: Url) -> Result<PathBuf, OdlError> {
     let resolver = ForcedResolver {};
 
     let instruction = dlm.evaluate(url, None, &resolver).await?;
